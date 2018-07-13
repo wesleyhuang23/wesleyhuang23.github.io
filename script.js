307169e095f8ee94f7b558f96cf7ed69359f5c71
Wesley <wesley@gooddogdesign.com>
2018-07-13 15:44:42 -0700

    added template

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 $(document).ready(function(){
-  particlesJS.load('particles-js', 'particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+  setTimeout(function(){
+    particlesJS.load('particles-js', 'particles.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
+  }, 500)
+  
 });
