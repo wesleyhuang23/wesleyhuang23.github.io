56026e879d109f424f6f9a253663888b5c1f5401
Wesley <wesley@gooddogdesign.com>
2018-07-13 16:19:51 -0700

    custom js fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,10 +4,10 @@
 	 * Preloader
 	/* ---------------------------------------------- */
 
-	$(window).load(function() {
-		$('#status').fadeOut();
-		$('#preloader').delay(350).fadeOut('slow');
-	});
+	// $(window).load(function() {
+	// 	$('#status').fadeOut();
+	// 	$('#preloader').delay(350).fadeOut('slow');
+	// });
 
 	$(document).ready(function() {
 
@@ -58,41 +58,41 @@
 		 * Twitter
 		/* ---------------------------------------------- */
 
-		var twitterConfig = {
-			id: '345170787868762112',
-			domId: '',
-			maxTweets: 3,
-			enableLinks: true,
-			showUser: false,
-			customCallback: handleTweets
-		};
+		// var twitterConfig = {
+		// 	id: '345170787868762112',
+		// 	domId: '',
+		// 	maxTweets: 3,
+		// 	enableLinks: true,
+		// 	showUser: false,
+		// 	customCallback: handleTweets
+		// };
 
-		twitterFetcher.fetch(twitterConfig);
+		// twitterFetcher.fetch(twitterConfig);
 
-		function handleTweets(tweets) {
-			var x = tweets.length;
-			var n = 0;
-			var html = '';
-			while(n < x) {
-				html += '<div class="owl-item">' + tweets[n] + '</div>';
-				n++;
-			}
-			$('.testimonials').html(html);
+		// function handleTweets(tweets) {
+		// 	var x = tweets.length;
+		// 	var n = 0;
+		// 	var html = '';
+		// 	while(n < x) {
+		// 		html += '<div class="owl-item">' + tweets[n] + '</div>';
+		// 		n++;
+		// 	}
+		// 	$('.testimonials').html(html);
 
-			$('.twitter_retweet_icon').html('<i class="fa fa-retweet"></i>');
-			$('.twitter_reply_icon').html('<i class="fa fa-reply"></i>');
-			$('.twitter_fav_icon').html('<i class="fa fa-star"></i>');
+		// 	$('.twitter_retweet_icon').html('<i class="fa fa-retweet"></i>');
+		// 	$('.twitter_reply_icon').html('<i class="fa fa-reply"></i>');
+		// 	$('.twitter_fav_icon').html('<i class="fa fa-star"></i>');
 
-			$('.testimonials').owlCarousel({
-				singleItem: true,
-				navigation: false,
-				pagination: false,
-				slideSpeed : 300,
-				paginationSpeed : 400,
-				autoPlay: 5000,
-				navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
-			});
-		}
+		// 	$('.testimonials').owlCarousel({
+		// 		singleItem: true,
+		// 		navigation: false,
+		// 		pagination: false,
+		// 		slideSpeed : 300,
+		// 		paginationSpeed : 400,
+		// 		autoPlay: 5000,
+		// 		navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+		// 	});
+		// }
 
 		/* ---------------------------------------------- /*
 		 * Initialize shuffle plugin
@@ -199,32 +199,32 @@
 		 * Google Map
 		/* ---------------------------------------------- */
 
-		var mapLocation = new google.maps.LatLng(34.031428,-118.2071542,17);
+		// var mapLocation = new google.maps.LatLng(34.031428,-118.2071542,17);
 
-		map = new GMaps({
-			streetViewControl : false,
-			overviewMapControl: false,
-			mapTypeControl: false,
-			zoomControl : false,
-			panControl : false,
-			scrollwheel: false,
-			center: mapLocation,
-			el: '#map',
-			zoom: 16,
-			styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
-		});
+		// map = new GMaps({
+		// 	streetViewControl : false,
+		// 	overviewMapControl: false,
+		// 	mapTypeControl: false,
+		// 	zoomControl : false,
+		// 	panControl : false,
+		// 	scrollwheel: false,
+		// 	center: mapLocation,
+		// 	el: '#map',
+		// 	zoom: 16,
+		// 	styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+		// });
 
-		var image = new google.maps.MarkerImage('assets/images/map-icon.png',
-			new google.maps.Size(80, 80),
-			new google.maps.Point(0, 0),
-			new google.maps.Point(40, 40)
-		);
+		// var image = new google.maps.MarkerImage('assets/images/map-icon.png',
+		// 	new google.maps.Size(80, 80),
+		// 	new google.maps.Point(0, 0),
+		// 	new google.maps.Point(40, 40)
+		// );
 
-		map.addMarker({
-			position: mapLocation,
-			icon: image,
-			animation: google.maps.Animation.BOUNCE,
-		});
+		// map.addMarker({
+		// 	position: mapLocation,
+		// 	icon: image,
+		// 	animation: google.maps.Animation.BOUNCE,
+		// });
 
 	});
 
--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 
 $(document).ready(function(){
-  setTimeout(function(){
     particlesJS.load('particles-js', 'particles.json', function() {
       console.log('callback - particles.js config loaded');
     });
-  }, 500)
-  
 });
